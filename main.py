3eeca39446ef69faecdb601bb7e0cba404a97c31
Wlad Kowalew <wladickowalew@gmail.com>
2025-02-20 16:35:07 +0300

    ttt commit

--- main.py
+++ main.py
@@ -1,8 +1,17 @@
-print('1111111')
-print('2222222')
-print('3333333')
-print('4')
-print('5')
-
-
-print('CCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCCC')
+ans = []
+while True:
+    line = input()
+    if line == '</body>':
+        break
+    if line[:3] == '<p>' and line[-4:] == '</p>':
+        ans.append(line[3:-4])
+    elif line[:3] == '<p>':
+        s = line[3:]
+        line = input()
+        while line[-4:] != '</p>':
+            s += ' ' + line
+            line = input()
+        s += ' ' + line[:-4]
+        ans.append(s)
+for i in range(len(ans) - 1, -1, -1):
+    print(ans[i])
